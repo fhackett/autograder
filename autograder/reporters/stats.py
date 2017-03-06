64c233a28b32f434a556dbf4cf32ff976c79d829
Finn Hackett <finn.hackett@maintool.me>
2017-03-06 07:11:04 -0800

    Added readability feature to stats output

--- autograder/reporters/stats.py
+++ autograder/reporters/stats.py
@@ -17,12 +17,14 @@ class StatsReporter(_autograder.Reporter):
     def __init__(self, operations):
         self.operations = operations
         self.accumulators = {op.name: [] for op in operations}
+        self.item_count = 0
     def on_individual_completion(self, id, success, data, global_data):
         if success:
+            self.item_count += 1
             for op in self.operations:
                 self.accumulators[op.name].append(op.read(data, global_data))
     def on_completion(self, data):
-        print('Statistics:')
+        print('Statistics (of {} successful items):'.format(self.item_count))
         for op in self.operations:
             print('{name}: {value}'.format(
                 name=op.name,
